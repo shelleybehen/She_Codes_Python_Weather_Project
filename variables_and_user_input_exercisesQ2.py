156b4cdce9f790e589cf1e917eeb9d7512bb28a1
shelleybehen <shelleybehen@gmail.com>
2021-07-10 09:30:12 +1000

    Update variables_and_user_input_exercisesQ2.py

--- variables_and_user_input_exercisesQ2.py
+++ variables_and_user_input_exercisesQ2.py
@@ -1,4 +1,4 @@
 number1 = input("Enter a number")
 number2 = input("Enter another number")
-product = int(number1) * int(number2)
-print(f"{number1} times {number2} equals {product} ")
+product = float(number1) * float(number2)
+print(f"{number1} * {number2} = {product} ")
